e234a0847fcfe5bfdea5f4fc11e9d7542ae9c43b
mrafey292 <mrafey292@gmail.com>
2025-04-27 16:22:07 +0500

    feat/ added categories api route

---
+++ pages/api/categories.js
@@ -0,0 +1,37 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+export default async function handle(req, res) {
+  const { method } = req;
+  await mongooseConnect();
+
+  if (method === "GET") {
+    res.json(await Category.find().populate("parent"));
+  }
+
+  if (method === "POST") {
+    const { name, parentCategory, properties } = req.body;
+
+    const CategoryDoc = await Category.create({
+      name,
+      parent: parentCategory || undefined,
+      properties,
+    });
+    res.json(CategoryDoc);
+  }
+
+  if (method === "PUT") {
+    const { name, parentCategory, properties, _id } = req.body;
+
+    const CategoryDoc = await Category.updateOne(
+      { _id },
+      { name, parent: parentCategory || undefined, properties }
+    );
+    res.json(CategoryDoc);
+  }
+
+  if (method === "DELETE") {
+    const { _id } = req.query;
+    await Category.deleteOne({ _id });
+    res.json("ok");
+  }
+}
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,44 +1,53 @@
-import { mongooseConnect } from '@/lib/mongoose';
-import { Product } from '@/models/Product';
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
 export default async function handle(req, res) {
-    const {method} = req;
-    await mongooseConnect();  
-    
-    if (method === "GET") {
-        if (req.query?.id) {
-            const productDoc = await Product.findOne({_id: req.query.id});
-        } else {
-            res.json(await Product.find());
-        }
-    }
+  const { method } = req;
+  await mongooseConnect();
 
-    if (method === "POST") {
-        const {title, description, price, images} = req.body;
-        const productDoc = await Product.create({
-            title,
-            description,
-            price,
-            images
-        })
-        res.json(productDoc);
+  if (method === "GET") {
+    if (req.query?.id) {
+      const productDoc = await Product.findOne({ _id: req.query.id });
+    } else {
+      res.json(await Product.find());
     }
+  }
 
-    if (method === "PUT") {
-        const {title, description, price, images, _id} = req.body;
-        await Product.updateOne({_id}, {
-            title,
-            description,
-            price,
-            images
-        })
-        res.json(true);
-    }
+  if (method === "POST") {
+    const { title, description, price, images, category, properties } =
+      req.body; //MADE CHANGES HERE
+    const productDoc = await Product.create({
+      title,
+      description,
+      price,
+      images,
+      category,
+      properties,
+    });
+    res.json(productDoc);
+  }
 
-    if (method === "DELETE") {
-        if (req.query?.id) {
-            await Product.deleteOne({_id: req.query.id});
-            res.json(true);
-        }
+  if (method === "PUT") {
+    const { title, description, price, images, category, properties, _id } =
+      req.body; //CHANGES HERE
+    await Product.updateOne(
+      { _id },
+      {
+        title,
+        description,
+        price,
+        images,
+        category,
+        properties,
+      }
+    );
+    res.json(true);
+  }
+
+  if (method === "DELETE") {
+    if (req.query?.id) {
+      await Product.deleteOne({ _id: req.query.id });
+      res.json(true);
     }
+  }
 }
